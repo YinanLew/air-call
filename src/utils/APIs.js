ab9a68b6fdb8f996a6afc11c8c8231dd05672a68
Yinan <104224746+YinanLew@users.noreply.github.com>
2022-12-14 17:22:47 -0500

    Update APIs.js
    Modified the APIs

--- src/utils/APIs.js
+++ src/utils/APIs.js
@@ -1,4 +1,4 @@
-export const ACTIVITY_FEED = "https://aircall-job.herokuapp.com/activities";
-export const USER_DETAILS = "https://aircall-job.herokuapp.com/activities/";
-export const ARCHIVE_CALL = "https://aircall-job.herokuapp.com/activities/";
-export const RESET_ALL_CALLS = "https://aircall-job.herokuapp.com/reset";
+export const ACTIVITY_FEED = "https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities";
+export const USER_DETAILS = "https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities";
+export const ARCHIVE_CALL = "https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities";
+export const RESET_ALL_CALLS = "https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/reset";
