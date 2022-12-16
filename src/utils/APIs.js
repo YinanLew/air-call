6ef017c1da70a50b246dab167c769c624ae04ad6
Yinan <104224746+YinanLew@users.noreply.github.com>
2022-12-16 14:23:17 -0500

    Update APIs.js
    modified the USER_DETAILS API

--- src/utils/APIs.js
+++ src/utils/APIs.js
@@ -1,4 +1,4 @@
 export const ACTIVITY_FEED = "https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities";
-export const USER_DETAILS = "https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities";
+export const USER_DETAILS = "https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities/";
 export const ARCHIVE_CALL = "https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities";
 export const RESET_ALL_CALLS = "https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/reset";
